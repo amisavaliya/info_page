de0ae688f0b7dee413f3e3a0d782f87226052cda
amisavaliya <61430198+amisavaliya@users.noreply.github.com>
2021-06-09 13:45:19 +0530

    Add files via upload

---
+++ AppColors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+
+  static const Color primaryColor= const Color(0Xff1167b1);
+  static const Color greenColor= const Color(0Xff60b000);
+  static const Color blackColor= const Color(0Xff000000);
+  static const Color whiteColor= const Color(0XffFFFFFF);
+}
